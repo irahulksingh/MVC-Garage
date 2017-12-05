266bf94c5df671192c22eecae3e6fb9ef4758bf7
meRahulSingh <mr.rahulsingh@hotmail.com>
2017-12-05 09:10:56 +0100

    Created Model

---
+++ GarageMVC/GarageMVC/Models/VehiclesModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GarageMVC.Models
+{
+    public class VehiclesModel
+    {
+        public int ID { get; set; }
+        public string Type { get; set; }
+        public string RegNo { get; set; }
+        public string Brand { get; set; }
+        public int NoOfWheels { get; set; }
+        public DateTime TimeIn { get; set; }
+        public DateTime TimeOut { get; set; }
+        public string AmountPayable { get; set; }
+    }
+}
