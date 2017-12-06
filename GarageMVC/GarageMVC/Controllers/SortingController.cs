aca076cf570d9b3e074971aadd2fe0f40f86bd7f
meRahulSingh <mr.rahulsingh@hotmail.com>
2017-12-06 17:14:05 +0100

    Sorting done. for the day.

---
+++ GarageMVC/GarageMVC/Controllers/SortingController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GarageMVC.DataAccessLayer;
+using GarageMVC.Models;
+
+namespace GarageMVC.Controllers
+{
+    public class SortingController : Controller
+    {
+        private DatabaseConnection db = new DatabaseConnection();
+
+        // GET: Sorting
+        public ActionResult Index(string SortOrder)
+        {
+            ViewBag.NameSortParm = String.IsNullOrEmpty(SortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = SortOrder == "Date" ? "date_desc" : "Date";
+            var vehicle = from s in db.VehiclesModel
+                          select s;
+            switch (SortOrder)
+            {
+                case "name_desc":
+                    vehicle = vehicle.OrderByDescending(s => s.RegNo);
+                    break;
+                case "Date":
+                    vehicle = vehicle.OrderByDescending(s => s.Model);
+                    break;
+                case "date_desc":
+                    vehicle = vehicle.OrderByDescending(s => s.Brand);
+                    break;
+                default:
+                    vehicle = vehicle.OrderByDescending(s => s.Type);
+                    break;
+            }
+            return View(vehicle.ToList());
+            //return View(db.VehiclesModel.ToList());
+        }
+
+        // GET: Sorting/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VehiclesModel vehiclesModel = db.VehiclesModel.Find(id);
+            if (vehiclesModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehiclesModel);
+        }
+
+        // GET: Sorting/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sorting/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Type,RegNo,Color,Model,Brand,NoOfWheels,CheckInTime,CheckOutTime")] VehiclesModel vehiclesModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.VehiclesModel.Add(vehiclesModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(vehiclesModel);
+        }
+
+        // GET: Sorting/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VehiclesModel vehiclesModel = db.VehiclesModel.Find(id);
+            if (vehiclesModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehiclesModel);
+        }
+
+        // POST: Sorting/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Type,RegNo,Color,Model,Brand,NoOfWheels,CheckInTime,CheckOutTime")] VehiclesModel vehiclesModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vehiclesModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(vehiclesModel);
+        }
+
+        // GET: Sorting/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VehiclesModel vehiclesModel = db.VehiclesModel.Find(id);
+            if (vehiclesModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehiclesModel);
+        }
+
+        // POST: Sorting/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            VehiclesModel vehiclesModel = db.VehiclesModel.Find(id);
+            db.VehiclesModel.Remove(vehiclesModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
